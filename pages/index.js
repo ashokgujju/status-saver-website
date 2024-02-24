b5fe5196888951417f2dbe9a9093bb57ccba4bd0
Ashok Kumar Gujju <ashokg.geek@gmail.com>
2024-02-24 21:51:05 +0530

    Add privacy policy

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,30 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Status Saver App" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Download and Share Your Friends WhatsApp Status Easily
         </p>
       </main>
+      <h1>Privacy Policy</h1>
+        <p>
+        This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used.
+        </p>
 
+        <h2>What personal information do we collect from the people that visit our blog, website or app?</h2>
+        <p> We do not collect any personal information from the user.</p>
+
+        <h2>Third-party disclosure</h2>
+        <p>We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information.</p>
+
+
+        <h2>Regarding the Data</h2>
+        <p>We store the last typed text in the app in local db on the device, we do not upload it to external servers.</p>
+
+
+        <h2>Contacting Us</h2>
+        <p>If there are any questions regarding this privacy policy, please email us on onescreenapps@gmail.com</p>
+        <p>Last Edited on 2024-02-24</p> 
       <Footer />
     </div>
   )
